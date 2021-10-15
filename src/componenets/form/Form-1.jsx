45f44c9ae777bfeb5a417cc238904eb918ccec71
pratheesh <pratheesh1269@gmail.com>
2021-10-15 10:22:55 +0800

    added buttons in from

--- src/componenets/form/Form-1.jsx
+++ src/componenets/form/Form-1.jsx
@@ -20,10 +20,39 @@ function Form1() {
               {/* non-responsive pagination */}
               <div className="row p-0 m-0 pagination">
                 <div className="row p-0 m-0"></div>
-                <Pagination page={"page5"} />
+                <Pagination page={"page1"} />
                 <hr className="bg-secondary form-hr" />
               </div>
-              <div className="row p-0 m-0"></div>
+              {/* Buttons */}
+              <div className="row p-0 m-0">
+                {/* TODO: Start here with buttons */}
+                <div class="col-12 col-md-6 bg-success">Hello</div>
+                <div class="col-12 col-md-6 bg-warning">Hello</div>
+              </div>
+              <div className="row m-0">
+                <div class="col-6 d-flex justify-content-start">
+                  <button
+                    type="button"
+                    class="btn my-2 btn-cancel align-middle"
+                  >
+                    Cancel
+                  </button>
+                </div>
+                <div class="col-6 d-flex justify-content-end">
+                  <button
+                    type="button"
+                    class="btn my-2 btn-previous align-middle"
+                  >
+                    Previous
+                  </button>
+                  <button
+                    type="button"
+                    className="btn my-2 btn-continue align-middle"
+                  >
+                    Continue
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
