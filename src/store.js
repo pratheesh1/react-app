594b3ecf9411f549a565ab9d1a76a6da1bdbe6c5
pratheesh <pratheesh1269@gmail.com>
2021-10-17 09:53:52 +0800

    add redux devtool support middleware

--- src/store.js
+++ src/store.js
@@ -1,16 +1,19 @@
 import create from "zustand";
+import { devtools } from "zustand/middleware";
 
 const api_base_url = "http://localhost:3500";
 const countries_api_endpoint = api_base_url + "/countries";
 
-const useCustomStore = create((set) => ({
-  countries: [],
-  fetchCountries: async () => {
-    var response = await fetch(countries_api_endpoint).catch((err) =>
-      console.error(err)
-    );
-    set({ countries: await response.json() });
-  },
-}));
+const useCustomStore = create(
+  devtools((set) => ({
+    countries: [],
+    fetchCountries: async () => {
+      var response = await fetch(countries_api_endpoint).catch((err) =>
+        console.error(err)
+      );
+      set({ countries: await response.json() });
+    },
+  }))
+);
 
 export default useCustomStore;
