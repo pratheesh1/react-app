af20d56ed97eccc439ddf3e270130e184c2390e3
pratheesh <pratheesh1269@gmail.com>
2021-10-22 06:53:21 +0800

    updated form page 1 to use react-hook-form

--- src/componenets/form/Form-1.jsx
+++ src/componenets/form/Form-1.jsx
@@ -4,6 +4,7 @@ import Pagination from "./Pagination";
 import Cancel from "./Cancel";
 import FormType from "./Form-Type";
 import { useFormStore } from "../../store";
+import { useForm } from "react-hook-form";
 
 function Form1(props) {
   const { firstName, lastName, email, aboutYou } = useFormStore(
@@ -12,116 +13,115 @@ function Form1(props) {
   const updateForm = useFormStore((state) => state.updateForm);
   const formNextPage = useFormStore((state) => state.formNextPage);
 
-  //update form
-  const updateFormData = (target, value) => {
-    updateForm(target, value);
+  const { register, handleSubmit, watch, error } = useForm();
+
+  watch((data) => {
+    for (const key in data) {
+      updateForm(key, data[key]);
+    }
+  });
+
+  const onContinue = (data) => {
+    try {
+      //form validation here
+      formNextPage();
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
     <React.Fragment>
       <div className="row w-100 p-0 m-0">
-        <div className="container form-wrapper">
-          <div className="container from-content-wrapper">
-            <div className="row p-0 m-0">
-              {/* Heading */}
-              <FormType type={props.type} />
-              {/* non-responsive pagination */}
-              <div className="row p-0 m-0 pagination">
-                <div className="row p-0 m-0"></div>
-                <Pagination page={1} />
-                <hr className="bg-secondary form-hr" />
-              </div>
+        <form onSubmit={handleSubmit(onContinue)}>
+          <div className="container form-wrapper">
+            <div className="container from-content-wrapper">
               <div className="row p-0 m-0">
-                {/* Form */}
-                <div className="col-12 p-0">
-                  <div className="row p-0 m-0 my-lg-3 form-row">
-                    <div className="col-12 col-lg-6 my-3 my-lg-0">
-                      <p className="field-heading m-0">first name</p>
-                      <input
-                        type="text"
-                        id="firstName"
-                        className="filed-input-small"
-                        placeholder="First name"
-                        value={firstName}
-                        onChange={(e) =>
-                          updateFormData(e.target.id, e.target.value)
-                        }
-                      />
+                {/* Heading */}
+                <FormType type={props.type} />
+                {/* non-responsive pagination */}
+                <div className="row p-0 m-0 pagination">
+                  <div className="row p-0 m-0"></div>
+                  <Pagination page={1} />
+                  <hr className="bg-secondary form-hr" />
+                </div>
+                <div className="row p-0 m-0">
+                  {/* Form */}
+                  <div className="col-12 p-0">
+                    <div className="row p-0 m-0 my-lg-3 form-row">
+                      <div className="col-12 col-lg-6 my-3 my-lg-0">
+                        <p className="field-heading m-0">first name</p>
+                        <input
+                          type="text"
+                          {...register("firstName")}
+                          className="filed-input-small"
+                          placeholder="First name"
+                          value={firstName}
+                        />
+                      </div>
+                      <div className="col-12 col-lg-6 my-3 my-lg-0">
+                        <p className="field-heading m-0">last name</p>
+                        <input
+                          type="text"
+                          {...register("lastName")}
+                          className="filed-input-small"
+                          placeholder="Last name"
+                          value={lastName}
+                        />
+                      </div>
                     </div>
-                    <div className="col-12 col-lg-6 my-3 my-lg-0">
-                      <p className="field-heading m-0">last name</p>
-                      <input
-                        type="text"
-                        id="lastName"
-                        className="filed-input-small"
-                        placeholder="Last name"
-                        value={lastName}
-                        onChange={(e) =>
-                          updateFormData(e.target.id, e.target.value)
-                        }
-                      />
+                    <div className="row p-0 m-0 my-lg-3 form-row">
+                      <div className="col-12 col-lg-6 my-3 my-lg-0">
+                        <p className="field-heading m-0">email</p>
+                        <input
+                          type="text"
+                          {...register("email")}
+                          className="filed-input-small"
+                          placeholder="Email ID"
+                          value={email}
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="row p-0 m-0 my-lg-3 form-row">
-                    <div className="col-12 col-lg-6 my-3 my-lg-0">
-                      <p className="field-heading m-0">email</p>
-                      <input
-                        type="text"
-                        id="email"
-                        className="filed-input-small"
-                        placeholder="Email ID"
-                        value={email}
-                        onChange={(e) =>
-                          updateFormData(e.target.id, e.target.value)
-                        }
-                      />
-                    </div>
-                  </div>
-                  <div className="row p-0 m-0 my-lg-3 form-row">
-                    <div className="col-12 col-lg-6 my-3 my-lg-0">
-                      <p className="field-heading m-0">
-                        tell us more about you
-                      </p>
-                      <textarea
-                        id="aboutYou"
-                        className="filed-input-large"
-                        placeholder="I am very into hiking and trail running. I enjoy connecting with..."
-                        value={aboutYou}
-                        onChange={(e) =>
-                          updateFormData(e.target.id, e.target.value)
-                        }
-                      />
+                    <div className="row p-0 m-0 my-lg-3 form-row">
+                      <div className="col-12 col-lg-6 my-3 my-lg-0">
+                        <p className="field-heading m-0">
+                          tell us more about you
+                        </p>
+                        <textarea
+                          {...register("aboutYou")}
+                          className="filed-input-large"
+                          placeholder="I am very into hiking and trail running. I enjoy connecting with..."
+                          value={aboutYou}
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="row m-0">
-                {/* Buttons */}
-                <div className="col-6 d-flex justify-content-start">
-                  <button
-                    type="button"
-                    className="btn my-2 btn-cancel align-middle"
-                    data-bs-toggle="modal"
-                    data-bs-target="#cancelModal"
-                  >
-                    Cancel
-                  </button>
-                </div>
-                <div className="col-6 d-flex justify-content-end">
-                  <button
-                    type="button"
-                    className="btn my-2 btn-continue align-middle"
-                    onClick={() => {
-                      formNextPage();
-                    }}
-                  >
-                    Continue
-                  </button>
+                <div className="row m-0">
+                  {/* Buttons */}
+                  <div className="col-6 d-flex justify-content-start">
+                    <button
+                      type="button"
+                      className="btn my-2 btn-cancel align-middle"
+                      data-bs-toggle="modal"
+                      data-bs-target="#cancelModal"
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                  <div className="col-6 d-flex justify-content-end">
+                    <button
+                      type="submit"
+                      className="btn my-2 btn-continue align-middle"
+                    >
+                      Continue
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </form>
       </div>
       <Cancel />
     </React.Fragment>
