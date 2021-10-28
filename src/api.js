4fcc263c36f9012b3aa7fb5ade18d810bf420a25
pratheesh <pratheesh1269@gmail.com>
2021-10-28 20:37:39 +0800

    update url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
-const api_base_url = "http://ps-project02-express.herokuapp.com/";
-const trails_api_endpoint = "http://ps-project02-express.herokuapp.com/trails/";
+const api_base_url = "https://ps-project02-express.herokuapp.com/";
+const trails_api_endpoint =
+  "https://ps-project02-express.herokuapp.com/trails/";
 const countries_api_endpoint =
-  "http://ps-project02-express.herokuapp.com/countries";
+  "https://ps-project02-express.herokuapp.com/countries";
 
 export { api_base_url, trails_api_endpoint, countries_api_endpoint };
